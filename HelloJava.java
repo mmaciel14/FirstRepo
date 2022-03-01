61a80226da623bee7f4ce164f78142381f4cc15f
mmaciel14 <marisolmac@hotmail.com>
2022-02-28 22:46:46 -0600

    Primeros Cmabios en Hello Java con consola

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,6 @@
 public class HelloJava {
-	
+	//cambio 1 con consola git
+                  //cambio 2 con consola git
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
